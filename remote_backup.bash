175647653e0987664aa3753268e35263c9758014
Alexandre Kandalintsev <spam@messir.net>
2013-03-25 13:24:54 +0100

    remote_backup.bash: 90% rewrite: removed unneded functionality

--- remote_backup.bash
+++ remote_backup.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#TODO: check whatss happend on overquota
-#DONE:
-#what happend if connection broke? - nothing good
+#TODO: check what will happen on overquota
+#TODO: what happen if connection brake?
 
-BASE="/mnt/dim13"
-REMOTE="exe@dim13.org"
 PASSWORD="file:/root/.backup_enc_pass"
+TAR="tar --atime-preserve --numeric-owner -cjf -"
+ENCRYPT="openssl enc -aes-256-cbc -salt -pass $PASSWORD"
+SSHFS="sshfs -o Ciphers=arcfour"
 
 ## EXE BASHLIB VERSION=1.5 ##############################
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
@@ -19,18 +19,15 @@ print_red() { color_red; echo -e "$*"; color_white; }
 print_yellow() { color_yellow; echo -e "$*"; color_white; }
 print_green() { color_green; echo -e "$*"; color_white; }
 
-print_debug() { if [ $DEBUG != 0 ]; then echo -e "$*"; fi }
+print_debug() { if [[ $DEBUG != 0 ]]; then echo -e "$*"; fi }
 
 die() { print_red "$*"; exit 1; }
-doReboot() { print_red "$*"; shutdown -r now; }
 
-getLinHash() { python -c "import crypt; print crypt.crypt(\"${1}\",\"\$1\$`pwgen -n -c 8 1`\$\")"; }
 
 #stolen from http://www.linuxjournal.com/content/use-bash-trap-statement-cleanup-temporary-files
 declare -a on_exit_items
 
-function on_exit()
-{
+function on_exit() {
     for i in "${on_exit_items[@]}"
     do
         echo "on_exit: $i"
@@ -38,8 +35,7 @@ function on_exit()
     done
 }
 
-function add_on_exit()
-{
+function add_on_exit() {
     local n=${#on_exit_items[*]}
     on_exit_items[$n]="$*"
     if [[ $n -eq 0 ]]; then
@@ -49,41 +45,56 @@ function add_on_exit()
 }
 ## END OF BASHLIB########################################
 
+LOCALPATH=""
+REMOTEURL=""
+COMPRESSION="lzop"  # compression
+OPTIND=1
+while getopts "l:r:c:h:d:" opt; do
+  case $opt in
+    l)
+      LOCALPATH=$OPTARG
+      ;;
+    r)
+      REMOTEURL=$OPTARG
+      ;;
+    c)
+      COMPRESSION=$OPTARG
+      ;;
+    d)
+      DEBUG=1
+      ;;
+    h)
+      echo "Usage:"
+      echo "  $0 -l <local URI> -r <remote URL> [-c zip]"
+      echo "  example: $0 -l /home -r mybackups@backup.org: -c lzop"
+      exit
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
 
-#SANITY CHECK
-INFILE="$1"
-if [ "$INFILE" == "-" ]; then
-    test $# -ne 2 && die "this script accepts 2 parameters ('-' and dst filename)"
-    MODE="stdin"
-    OUTFILE="${BASE}/${2}-`date +%F`"
-else
-    test $# -ne 1 && die "this script accepts only 1 parameter (filename to copy)"
-    MODE="copy"
-    test -f "$INFILE"  || die "can't find file \"$INFILE\""
-    OUTFILE="${BASE}/`basename $1`_enc-`date +%F`"
+if [[ -z "$LOCALPATH" || -z "$REMOTEURL" ]]; then
+    die "I need two arguments: -l localpath-r remotepath"
 fi
 
+MNT="/mnt/`echo $REMOTEURL | cut -d: -f1 | cut -d@ -f2`"
+OUTFILE="$MNT/`basename $LOCALPATH`_`date +%F`"
+print_debug "OUTFILE: $OUTFILE"
 
-#REMOTE MOUNT
-sshfs -C arcfour $REMOTE: $BASE || die "cant mount sshfs"
-add_on_exit fusermount -u $BASE || die "cant unmount sshfs"
+print_debug "mounting sshfs"
+if [ !  -d $MNT ]; then
+    print_green "creating $MNT"
+    mkdir -p $MNT || die "cannot make a mount mount"
+fi
+$SSHFS $REMOTEURL $MNT || die "cannot mount sshfs"
+add_on_exit fusermount -u $MNT
 
-
-#UPLOAD FILE
+print_debug "uploading file"
 test -s "$OUTFILE" && die "output file already exists: \"$OUTFILE\""
-#to decode use "openssl enc -d -aes-256-cbc -in file.enc -out file.dec"
-if [ "$MODE" == "copy" ]; then
-    openssl enc -aes-256-cbc -salt -pass $PASSWORD -in "$INFILE" -out "$OUTFILE" || die "openssl error"
-    #VERIFY UPLOAD
-    FILESIZE=$(stat -c%s "$INFILE")
-    UPLOADSIZE=$(stat -c%s "$OUTFILE") || die "upload file error"
-    echo "ORIG: $FILESIZE; UPLOAD: $UPLOADSIZE"
-    if [ $UPLOADSIZE -lt $FILESIZE ]; then
-        die "uploaded file less than orig. At least check quota."
-    fi
-else
-    cat | openssl enc -aes-256-cbc -salt -pass $PASSWORD > "$OUTFILE" || die "openssl error"
-fi
-
-
+CMD="$TAR $LOCALPATH | $ENCRYPT > "$OUTFILE" || die \"openssl error\""
+print_debug "$CMD"
+$CMD
+print_debug "syncing filesystems"
 sync # this will never hurt (c) slackware. Actually it can :)
